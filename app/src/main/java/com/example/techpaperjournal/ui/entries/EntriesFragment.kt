2464a4c0e37793d7a28f85af3076a543a9e9ea47
Yousra Elassa <yousra.elassa40@bcmail.cuny.edu>
2025-03-16 16:23:53 -0400

    Connect view models & adapters to fragments

--- app/src/main/java/com/example/techpaperjournal/ui/entries/EntriesFragment.kt
+++ app/src/main/java/com/example/techpaperjournal/ui/entries/EntriesFragment.kt
@@ -1,4 +1,60 @@
 package com.example.techpaperjournal.ui.entries
 
-class EntriesFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.techpaperjournal.adapter.EntryAdapter
+import com.example.techpaperjournal.adapter.PaperAdapter
+import com.example.techpaperjournal.databinding.FragmentEntriesBinding
+import com.example.techpaperjournal.databinding.FragmentHomeBinding
+import com.example.techpaperjournal.ui.home.HomeViewModel
+import com.example.techpaperjournal.ui.papers.PapersViewModel
+import kotlinx.coroutines.launch
+
+class EntriesFragment : Fragment() {
+    private var _binding: FragmentEntriesBinding? = null
+
+    private lateinit var entriesRecyclerView: RecyclerView
+    private lateinit var entryAdapter: EntryAdapter
+    private lateinit var entriesViewModel: EntriesViewModel
+
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEntriesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        entriesViewModel = ViewModelProvider(this)[EntriesViewModel::class.java]
+        entriesRecyclerView = binding.entriesRv
+        entryAdapter = EntryAdapter(requireContext(), mutableListOf())
+        entriesRecyclerView.adapter = entryAdapter
+        entriesRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+
+
+        lifecycleScope.launch {
+            entriesViewModel.uiState.observe(viewLifecycleOwner) { uiState ->
+                entryAdapter.updateEntries(uiState.entries)
+            }
+        }
+        entriesViewModel.fetchEntries()
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/techpaperjournal/ui/papers/PapersFragment.kt
+++ app/src/main/java/com/example/techpaperjournal/ui/papers/PapersFragment.kt
@@ -1,4 +1,54 @@
 package com.example.techpaperjournal.ui.papers
 
-class PapersFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.techpaperjournal.adapter.PaperAdapter
+import com.example.techpaperjournal.databinding.FragmentPapersBinding
+import kotlinx.coroutines.launch
+
+class PapersFragment : Fragment() {
+    private var _binding: FragmentPapersBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+
+    private lateinit var papersRecyclerView: RecyclerView
+    private lateinit var paperAdapter: PaperAdapter
+    private lateinit var papersViewModel: PapersViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPapersBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        papersViewModel = ViewModelProvider(this)[PapersViewModel::class.java]
+        papersRecyclerView = binding.papersRv
+        paperAdapter = PaperAdapter(requireContext(), mutableListOf())
+        papersRecyclerView.adapter = paperAdapter
+        papersRecyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+
+        lifecycleScope.launch {
+            papersViewModel.uiState.observe(viewLifecycleOwner) { uiState ->
+                paperAdapter.updatePapers(uiState.papers)
+            }
+        }
+        papersViewModel.fetchPapers()
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
