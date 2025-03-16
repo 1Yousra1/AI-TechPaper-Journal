34364a916dd00b6ebfcea947b1d9e771824704a9
Yousra Elassa <yousra.elassa40@bcmail.cuny.edu>
2025-03-16 16:24:21 -0400

    Implement add dialog interactivity: dialogs open and file picker opens

--- app/src/main/java/com/example/techpaperjournal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/techpaperjournal/ui/home/HomeFragment.kt
@@ -1,17 +1,25 @@
 package com.example.techpaperjournal.ui.home
 
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.techpaperjournal.R
 import com.example.techpaperjournal.databinding.FragmentHomeBinding
+
+// Request code for selecting a PDF document.
+const val PICK_PDF_FILE = 2
 
 class HomeFragment : Fragment() {
 
@@ -26,20 +34,61 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         binding.addButton.setOnClickListener {
-            val customView = LayoutInflater.from(context).inflate(R.layout.custom_add_dialog, null)
+            val addDialogView = LayoutInflater.from(context).inflate(R.layout.custom_add_dialog, null)
             val dialog = AlertDialog.Builder(context)
-                .setView(customView)
+                .setView(addDialogView)
                 .create()
             dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             dialog.show()
+            addDialogView.findViewById<TextView>(R.id.add_entry_button).setOnClickListener {
+                dialog.dismiss()
+                val addEntryView = LayoutInflater.from(context).inflate(R.layout.custom_add_entry_dialog, null)
+                val entryDialog = AlertDialog.Builder(context)
+                    .setView(addEntryView)
+                    .create()
+                entryDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                entryDialog.show()
+            }
+            addDialogView.findViewById<TextView>(R.id.upload_paper_button).setOnClickListener {
+                openFile()
+                dialog.dismiss()
+                val addPaperView = LayoutInflater.from(context).inflate(R.layout.custom_upload_paper_dialog, null)
+                val paperDialog = AlertDialog.Builder(context)
+                    .setView(addPaperView)
+                    .create()
+                paperDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                paperDialog.show()
+            }
         }
 
         return root
+    }
+
+    private fun openFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/pdf"
+
+        }
+
+        startActivityForResult(intent, PICK_PDF_FILE)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK) {
+            resultData?.data?.also { uri ->
+                uploadPdf(uri)
+            }
+        }
+    }
+
+    private fun uploadPdf(pdfUri: Uri) {
+        Toast.makeText(requireContext(), "PDF selected: $pdfUri", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/techpaperjournal/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/techpaperjournal/ui/library/LibraryFragment.kt
@@ -1,12 +1,17 @@
 package com.example.techpaperjournal.ui.library
 
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +19,9 @@ import com.example.techpaperjournal.R
 import com.example.techpaperjournal.databinding.FragmentLibraryBinding
 import com.example.techpaperjournal.ui.entries.EntriesFragment
 import com.example.techpaperjournal.ui.papers.PapersFragment
+
+// Request code for selecting a PDF document.
+const val PICK_PDF_FILE = 2
 
 class LibraryFragment : Fragment() {
 
@@ -32,13 +40,40 @@ class LibraryFragment : Fragment() {
         val root: View = binding.root
 
         binding.addButton.setOnClickListener {
-            val customView = LayoutInflater.from(context).inflate(R.layout.custom_add_dialog, null)
+            val addDialogView = LayoutInflater.from(context).inflate(R.layout.custom_add_dialog, null)
             val dialog = AlertDialog.Builder(context)
-                .setView(customView)
+                .setView(addDialogView)
                 .create()
             dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             dialog.show()
+            addDialogView.findViewById<TextView>(R.id.add_entry_button).setOnClickListener {
+                dialog.dismiss()
+                val addEntryView = LayoutInflater.from(context).inflate(R.layout.custom_add_entry_dialog, null)
+                val entryDialog = AlertDialog.Builder(context)
+                    .setView(addEntryView)
+                    .create()
+                entryDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                entryDialog.show()
+            }
+            addDialogView.findViewById<TextView>(R.id.upload_paper_button).setOnClickListener {
+                openFile()
+                dialog.dismiss()
+                val addPaperView = LayoutInflater.from(context).inflate(R.layout.custom_upload_paper_dialog, null)
+                val paperDialog = AlertDialog.Builder(context)
+                    .setView(addPaperView)
+                    .create()
+                paperDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                paperDialog.show()
+            }
         }
+
+        binding.searchButton.setOnClickListener {
+            binding.searchBar.visibility = View.VISIBLE
+            binding.root.setOnClickListener {
+                binding.searchBar.visibility = View.INVISIBLE
+            }
+        }
+
 
         replaceFragment(PapersFragment())
         binding.papersTab.setOnClickListener {
@@ -60,6 +95,29 @@ class LibraryFragment : Fragment() {
         childFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .commit()
+    }
+
+    private fun openFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/pdf"
+
+        }
+
+        startActivityForResult(intent, com.example.techpaperjournal.ui.home.PICK_PDF_FILE)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK) {
+            resultData?.data?.also { uri ->
+                uploadPdf(uri)
+            }
+        }
+    }
+
+    private fun uploadPdf(pdfUri: Uri) {
+        Toast.makeText(requireContext(), "PDF selected: $pdfUri", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
