44d66bf034e2dac4fd9f343b3027f7723328ba6a
Yousra Elassa <yousra.elassa40@bcmail.cuny.edu>
2025-05-08 05:05:08 -0400

    Separate paper layouts for Home and Library views
    - Created dedicated layouts for papers in Home and Library.
    - Enhanced PaperAdapter to handle different layouts based on context.

--- app/src/main/java/com/example/techpaperjournal/features/library/papers/PaperAdapter.kt
+++ app/src/main/java/com/example/techpaperjournal/features/library/papers/PaperAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.techpaperjournal.adapter
+package com.example.techpaperjournal.features.library.papers
 
 import android.content.Context
 import android.util.Log
@@ -8,13 +8,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.techpaperjournal.R
-import com.example.techpaperjournal.data.model.Paper
+import com.example.techpaperjournal.core.model.Paper
 
 class PaperAdapter (
     private val context: Context,
     private var papers: MutableList<Paper>,
     private val onPaperClick: (Paper) -> Unit,
-    private val onPaperLongClick: (Paper) -> Unit
+    private val onPaperLongClick: (Paper) -> Unit,
+    private val isFromHome: Boolean
 ) : RecyclerView.Adapter<PaperAdapter.PaperViewHolder>() {
     // Add a new paper to the list and notify the adapter
     fun updatePapers(newPapers: List<Paper>) {
@@ -23,9 +24,14 @@ class PaperAdapter (
     }
 
     // Inflate the item layout for each item and return a new ViewHolder
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaperAdapter.PaperViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_paper, parent, false)
-        return PaperViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaperViewHolder {
+        if (isFromHome) {
+            val view = LayoutInflater.from(context).inflate(R.layout.item_paper_home, parent, false)
+            return PaperViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(context).inflate(R.layout.item_paper_library, parent, false)
+            return PaperViewHolder(view)
+        }
     }
 
     // Bind the data for the current item to the ViewHolder
@@ -43,11 +49,7 @@ class PaperAdapter (
      inner class PaperViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(paper: Paper) {
             itemView.findViewById<TextView>(R.id.paper_title).text = paper.title
-
-            itemView.setOnClickListener {
-                onPaperClick(paper)
-            }
-
+            itemView.setOnClickListener { onPaperClick(paper) }
             itemView.setOnLongClickListener {
                 onPaperLongClick(paper)
                 true
